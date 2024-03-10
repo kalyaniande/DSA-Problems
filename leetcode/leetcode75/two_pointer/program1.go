001eb3857188d204d2a408758e8cc04d7ab513b9
kalyaniande <a.lakshmikalyani@gmail.com>
2024-03-10 16:04:22 +0530

    two pointer approach problems

---
+++ leetcode/leetcode75/two_pointer/program1.go
@@ -0,0 +1,44 @@
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
+
+// Example 1:
+
+// Input: nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+// Example 2:
+
+// Input: nums = [0]
+// Output: [0]
+
+// Constraints:
+
+// 1 <= nums.length <= 10 pow 4
+// -2 pow 31 <= nums[i] <= 2 pow 31 - 1
+
+// Follow up: Could you minimize the total number of operations done?
+
+package main
+
+import (
+	"fmt"
+)
+
+func moveZeroes(nums []int) {
+	j := 0
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != 0 {
+			nums[i], nums[j] = nums[j], nums[i]
+			j++
+		}
+	}
+}
+
+func main() {
+	nums := []int{0, 1, 0, 3, 12}
+	moveZeroes(nums)
+	fmt.Println("Output: ", nums)
+}
+
+// Time Complexity: O(N)
+// Space Complexity: O(1)
